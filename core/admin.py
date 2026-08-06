e3bcb6732c73932740f27c0075405a86000fb9e0
Saranya <saranya.po2004@gmail.com>
2026-08-06 23:07:31 +0530

    Day 8: register models in Django admin, verify view/edit access

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Accident, HotspotCluster
 
-# Register your models here.
+class AccidentAdmin(admin.ModelAdmin):
+    list_display = ('accident_id', 'city', 'state', 'date', 'accident_severity', 'custom_risk_score')
+    list_filter = ('city', 'accident_severity', 'weather')
+    search_fields = ('city', 'accident_id')
+
+class HotspotClusterAdmin(admin.ModelAdmin):
+    list_display = ('city', 'cluster_id', 'risk_level', 'avg_risk_score', 'accident_count', 'center_lat', 'center_lng')
+    list_filter = ('city', 'risk_level')
+
+admin.site.register(Accident, AccidentAdmin)
+admin.site.register(HotspotCluster, HotspotClusterAdmin)
